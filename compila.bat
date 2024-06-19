e6a75ecba90102ffd71a3134ea8d96979d42dfcb
U-AGRI2000\Giovanni.zezza <Giovanni.zezza@PC-ZEZZA.agri2000.local>
2024-06-19 12:47:46 +0200

    Aggiunta regola .gitattributes per .ahk e compilazione per le due versioni

--- compila.bat
+++ compila.bat
@@ -1,2 +1,9 @@
+@ECHO OFF
+IF [%1%]==[au3] (
 "c:\Program Files (x86)\AutoIt3\Aut2Exe\Aut2exe_x64.exe" /in dividi_pdf_vta.au3 /out dividi_pdf_vta.exe
 copy dividi_pdf_vta.au3.ini dividi_pdf_vta.exe.ini
+)
+IF [%1%]==[ahk] (
+"c:\Program Files\AutoHotkey\Compiler\Ahk2Exe.exe" /in dividi_pdf_vta.ahk /base "c:\Program Files\AutoHotkey\v2\AutoHotkey64.exe" /out dividi_pdf_vta.exe
+copy dividi_pdf_vta.ahk.ini dividi_pdf_vta.exe.ini
+)
